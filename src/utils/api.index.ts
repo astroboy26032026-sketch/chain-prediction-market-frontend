e9e4a4151a1984f5cc23aa7b0dcfa209762db1c4
astroboy26032026-sketch <astroboy26032026@gmail.com>
2025-12-30 17:16:12 +0700

    a

--- src/utils/api.index.ts
+++ src/utils/api.index.ts
@@ -1,20 +1,43 @@
-/**
- * api.index.ts
- * Switch API layer theo NEXT_PUBLIC_USE_MOCK
- * An toàn cho Next.js (client + server)
- */
+// src/utils/api.index.ts
+
+import * as realApi from './api';
+import * as mockApi from './api.mock';
 
 const USE_MOCK =
   process.env.NEXT_PUBLIC_USE_MOCK === 'true';
 
-// ⚠️ QUAN TRỌNG:
-// dùng export * tĩnh, KHÔNG require động
-// nhưng tách file theo env NGAY LÚC LOAD MODULE
+const impl = USE_MOCK ? mockApi : realApi;
 
-if (USE_MOCK) {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  module.exports = require('./api.mock');
-} else {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  module.exports = require('./api');
-}
+/**
+ * ⚠️ RE-EXPORT NAMED FUNCTIONS
+ * giữ nguyên API cũ
+ * nhưng switch implementation theo env
+ */
+
+export const getAllTokensTrends = (...args: Parameters<typeof realApi.getAllTokensTrends>) =>
+  impl.getAllTokensTrends(...args);
+
+export const getRecentTokens = (...args: Parameters<typeof realApi.getRecentTokens>) =>
+  impl.getRecentTokens(...args);
+
+export const searchTokens = (...args: Parameters<typeof realApi.searchTokens>) =>
+  impl.searchTokens(...args);
+
+export const getTokensWithLiquidity = (...args: Parameters<typeof realApi.getTokensWithLiquidity>) =>
+  impl.getTokensWithLiquidity(...args);
+
+export const getTokenInfoAndTransactions = (
+  ...args: Parameters<typeof realApi.getTokenInfoAndTransactions>
+) => impl.getTokenInfoAndTransactions(...args);
+
+export const getTokenUSDPriceHistory = (
+  ...args: Parameters<typeof realApi.getTokenUSDPriceHistory>
+) => impl.getTokenUSDPriceHistory(...args);
+
+export const getTokenHolders = (
+  ...args: Parameters<typeof realApi.getTokenHolders>
+) => impl.getTokenHolders(...args);
+
+export const getTokenLiquidityEvents = (
+  ...args: Parameters<typeof realApi.getTokenLiquidityEvents>
+) => impl.getTokenLiquidityEvents(...args);
